11dae0f88d9bf72bab396fa790b91b2c0392a9bc
DobleTmatutin0 <zz_00024@hotmail.com>
2026-06-03 17:32:13 -0300

    import libraries

---
+++ trivial-file-transfer-protocol-TFTP/tftp-server.c
@@ -0,0 +1,18 @@
+
+// Librerias del estandar ANSI / ISO C
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <signal.h>
+
+// Librerias del estandar POSIX
+#include <sys/socket.h> // Berkeley sockets
+#include <arpa/inet.h> // functions for manipulate ip addresses (part of Berkeley sockets)
+#include <netinet/ip.h> // defines InternetProtocol and addess family (part of Berkeley sockets)
+#include <unistd.h> // syscalls
+#include <sys/types.h> // defines various data types
+
+// Direccion por defecto del servidor.
+#define PORT;
+#define IP;
+
